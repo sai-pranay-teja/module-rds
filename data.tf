7895adb26f8edcc9e1c55826ef8adccbb2dbe01b
saipranayteja <pranayteja.ch@algonox.com>
2023-06-21 20:22:39 +0530

    sdf

--- data.tf
+++ data.tf
@@ -1,11 +1,11 @@
 /* data "aws_kms_key" "roboshop-key" {
   key_id = "alias/roboshop"
-}
+} */
 
 data "aws_ssm_parameter" "user" {
-  name = "env.rds.user"
+  name = "dev.rds.user"
 }
 
 data "aws_ssm_parameter" "pass" {
-  name = "env.rds.pass"
-} */
+  name = "dev.rds.pass"
+}
--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "aws_rds_cluster" "rds" {
   engine                  = var.engine
   engine_version          = var.engine_version
   database_name           = var.database_name
-  master_username         = "admin"
-  master_password         = "Roboshop1"
+  master_username         = data.aws_ssm_parameter.user.value
+  master_password         = data.aws_ssm_parameter.pass.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   db_subnet_group_name=aws_db_subnet_group.rds-subnet.name
